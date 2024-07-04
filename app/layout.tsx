660f082a9288c713848e4540682082e3b7a38f87
Kyaw Thu <evanch98@gmail.com>
2024-07-04 18:10:42 +0800

    adding the Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/nav/navbar";
 
 const font = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
