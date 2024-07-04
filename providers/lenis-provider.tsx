73c11c8ec399bccef9be7b36eca4f9199bf9e8bb
Kyaw Thu <evanch98@gmail.com>
2024-07-05 00:51:31 +0800

    adding a provider for lenis for smooth scrolling

---
+++ providers/lenis-provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import ReactLenis from "lenis/react";
+
+interface LenisProviderProps {
+  children: React.ReactNode;
+}
+
+export const LenisProvider = ({ children }: LenisProviderProps) => {
+  return (
+    <ReactLenis
+      root
+      options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}
+    >
+      {children}
+    </ReactLenis>
+  );
+};
