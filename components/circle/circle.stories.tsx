30471afce0489a78c998f5e71976648f1d491fe8
Kyaw Thu <evanch98@gmail.com>
2024-07-04 23:36:33 +0800

    adding a new component Circle

---
+++ components/circle/circle.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Circle } from "./circle";
+
+const meta: Meta<typeof Circle> = {
+  title: "Components/Circle",
+  component: Circle,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component: "Circle component",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Circle>;
+
+export const Default: Story = {};
+
+export const Purple: Story = {
+  args: {
+    size: 150,
+    colorClass: "bg-purple-500",
+  },
+};
+
+export const Blue: Story = {
+  args: {
+    size: 50,
+    colorClass: "bg-blue-500",
+  },
+};
