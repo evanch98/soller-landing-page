905e7cee82ff011ba3c5dba399aa9361f78d825a
Kyaw Thu <evanch98@gmail.com>
2024-07-05 20:08:52 +0800

    adding the OverviewSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/sections/home/hero-section/hero-section";
+import { OverviewSection } from "@/components/sections/home/overview-section/overview-section";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <OverviewSection />
       <div className="h-screen"></div>
     </main>
   );
