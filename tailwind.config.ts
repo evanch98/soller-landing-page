ae08fc6c928d611f2c4c778d0f47401b19a103b5
Kyaw Thu <evanch98@gmail.com>
2024-07-06 20:56:06 +0800

    changing the screen breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        xl: "1440px",
+      },
     },
     boxShadow: {
       xs: "0 0 2px 0 rgb(0 0 0 / 0.07)",
