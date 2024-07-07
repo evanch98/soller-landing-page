5e48c4a4ff1aa80624e9f01a12278e9c38413cdc
Kyaw Thu <evanch98@gmail.com>
2024-07-07 13:38:06 +0800

    adding a new component for loading

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { TbTruckLoading } from "react-icons/tb";
+
+const Loading = () => {
+  return (
+    <div>
+      Loading... <TbTruckLoading />
+    </div>
+  );
+};
+
+export default Loading;
