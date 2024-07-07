d71fd82920dfe8a976224e79ff580923f70d26ef
Kyaw Thu <evanch98@gmail.com>
2024-07-07 22:18:49 +0800

    updating the Navbar

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/button/button";
 import { RiArrowRightLine, RiHeadphoneLine, RiMenuFill } from "react-icons/ri";
 import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 const routes = [
   {
@@ -27,15 +28,23 @@ const routes = [
 
 export const Navbar = () => {
   const [hidden, setHidden] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   const { scrollY } = useScroll();
 
   useMotionValueEvent(scrollY, "change", (latest) => {
     const previous = scrollY.getPrevious();
+    console.log(`Latest: ${latest}, Previous: ${previous}`);
     if (latest > previous! && latest > 150) {
       setHidden(true);
     } else {
       setHidden(false);
+    }
+
+    if (latest > 150) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
     }
   });
 
@@ -47,7 +56,10 @@ export const Navbar = () => {
       }}
       animate={hidden ? "hidden" : "visible"}
       transition={{ duration: 0.35, ease: "easeInOut" }}
-      className="sticky top-0 z-50 flex w-full items-center justify-between px-4 py-2 lg:px-20 lg:py-6"
+      className={cn(
+        "fixed top-0 z-[999] flex w-full items-center justify-between px-4 py-2 lg:px-20 lg:py-6",
+        scrolled && "bg-white shadow-md",
+      )}
     >
       <div className="flex items-center justify-center gap-x-9">
         <Link href="/">
