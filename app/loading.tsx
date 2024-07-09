cf9be6c9f491a4256cb8a26143230f3c9a3921a6
Kyaw Thu <evanch98@gmail.com>
2024-07-09 12:46:41 +0800

    adding a loading animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,9 @@
-import { TbTruckLoading } from "react-icons/tb";
+import { LuLoader2 } from "react-icons/lu";
 
 const Loading = () => {
   return (
-    <div>
-      Loading... <TbTruckLoading />
+    <div className="flex h-full w-full animate-spin flex-col items-center justify-center">
+      <LuLoader2 className="h-12 w-12 text-amber" />
     </div>
   );
 };
